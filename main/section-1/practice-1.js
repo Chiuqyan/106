a9213c3fd7d5dba208999c5a9029c34b7be02377
Chiu_yan <32241150+Chiuqyan@users.noreply.github.com>
2017-11-03 02:11:40 +0800

    another way
    about indexOf

--- main/section-1/practice-1.js
+++ main/section-1/practice-1.js
@@ -2,9 +2,9 @@
 
 module.exports = function collectSameElements(collectionA, collectionB) {
     var result= new Array();
-    for (var i=0;i < collectionA.length;i++)
+    for (var i=0;i < collectionA.length;i++)//for(var i 1n collectionA)
         {
-            for(var j=0 ;j < collectionB.length;j++)
+            for(var j=0 ;j < collectionB.length;j++)//if(collectionB.inexOf(i)!==-1)
             {
             if(collectionA[i] == collectionB[j])
             {
